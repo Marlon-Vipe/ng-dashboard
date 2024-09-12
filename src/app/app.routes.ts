ee3f832a2c4c6795e1864fc7100773e89ac5fc26
Marlon Villalona <iamvillalona@gmail.com>
2024-09-12 00:08:02 -0400

    dashboard separated without cards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./shared/components/layout/layout.component').then(c => c.LayoutComponent),
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./business/dashboard/dashboard.component').then(c => c.DashboardComponent)
+            },
+            {
+                path: 'profile',
+                loadComponent: () => import('./business/profile/profile.component').then(c => c.ProfileComponent)
+            },
+            {
+                path: 'tables',
+                loadComponent: () => import('./business/tables/tables.component').then(c => c.TablesComponent)
+            }
+        ]
+    }
+];
